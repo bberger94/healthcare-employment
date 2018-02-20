a304707989dd0535bb838d85190af8a80777970e
Ben Berger <benjaminaberger94@gmail.com>
2018-02-20 15:38:15 -0500

    Added new script to run_scripts.sh

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,2 +1,2 @@
-statamp  source/01_figures.do && statamp  source/02_regressions.do &
+statamp  source/01_figures.do && statamp  source/02_regressions.do && statamp source/03_percapita_analysis.do &
  
